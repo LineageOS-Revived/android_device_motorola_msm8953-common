44e489166ba036a540099c4153e5a125cafb6081
hpnightowl <iamhp2k@gmail.com>
2022-02-09 05:27:48 +0530

    msm8953-common: drop audio.a2dp.default
    - building from here
      https://github.com/LineageOS/android_build/blob/lineage-18.1/target/product/full_base.mk#L35
    
    Change-Id: Ib71c1abd381468ec3621d60e5ca959f738f51168
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- msm8953.mk
+++ msm8953.mk
@@ -35,7 +35,6 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.2-impl_32 \
     android.hardware.soundtrigger@2.2-service \
     audiod \
-    audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
     libaacwrapper \
